96894e7d0597478b75b278810accf0ff90cd8baf
kien_hitek <kiennguyen@hitek.com.vn>
2024-03-23 23:17:24 +0700

    add file build

---
+++ build/assets/authApi-FT42z5FG.js
@@ -0,0 +1,1 @@
+import{a,R as t}from"./axiosClient-BIXGjfOH.js";const i=async s=>await a.post(`${t.AUTH}/login`,s),r=async s=>await a.post(`${t.AUTH}/register`,s);export{i as l,r};
---
+++ build/assets/index-BWBikUGW.js
@@ -0,0 +1,1 @@
+const n=r=>Object.fromEntries(Object.entries(r).map(([t,e])=>[t,JSON.stringify(e)]));export{n as c};
